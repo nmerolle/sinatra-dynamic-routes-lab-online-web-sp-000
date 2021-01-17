fc971758b1a9c7262b4859b0bf0df28f85344b4a
nmerolle <nmerolle11@hotmail.com>
2021-01-17 04:55:20 -0500

    commit tree 26d3f2a0b12f72e60f610739cc532b78690fe7f4

--- app.rb
+++ app.rb
@@ -2,5 +2,41 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+   @rev_name = params[:name].reverse
+   "#{@rev_name}"
+  end
 
+  get '/square/:number' do
+    @square = (params[:number].to_i)**2
+    "#{@square}"
+  end
+
+  get '/say/:number/:phrase' do
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @statement =""
+    @num.times do 
+     @statement  += "#{@phrase}\n"
+    end
+    "#{@statement}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+  @num1 = params[:number1].to_i
+  @num2 = params[:number2].to_i
+    if params[:operation] == "add"
+      "#{@num1+@num2}"
+    elsif params[:operation] == "subtract"
+      "#{@num1-@num2}"
+    elsif params[:operation] == "multiply"
+      "#{@num1*@num2}"
+    elsif params[:operation] == "divide"
+      "#{@num1/@num2}"
+    end
+  end
 end
